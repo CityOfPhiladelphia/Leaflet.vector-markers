6e07afd6a5f9b251a533026edc83855d3c97c4d5
andy.rothwell <rothwell.andy@gmail.com>
2018-03-23 15:30:42 -0400

    Update example to use exports

--- examples/js/index.js
+++ examples/js/index.js
@@ -1,15 +1,16 @@
-import { VectorMarkers } from '../../src';
-import css from "!style!css!sass!../../src/leaflet-vector-markers.scss"
+import { Map, TileLayer, Marker } from 'leaflet';
+import { VectorMarker, Icon } from '../../src';
+import '!style!css!../../src/leaflet-vector-markers.css';
 
-var map = L.map('map').setView([48.15491,11.54183], 14)
+const map = new Map('map').setView([48.15491,11.54183], 14)
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+new TileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
   detectRetina: true
 }).addTo(map)
 
-L.marker([48.15491,11.54183], {icon: L.VectorMarkers.icon({icon: 'spinner', prefix: 'fa', markerColor: '#cb4b16', spin: true}) }).addTo(map)
+new Marker([48.15491,11.54183], {icon: new Icon({icon: 'spinner', prefix: 'fa', markerColor: '#cb4b16', spin: true}) }).addTo(map)
 
-L.marker([48.15391,11.53283], {icon: L.VectorMarkers.icon({icon: 'rocket', prefix: 'fa', markerColor: '#002b36', iconColor: '#eee8d5'}) }).addTo(map)
+new Marker([48.15391,11.53283], {icon: new Icon({icon: 'rocket', prefix: 'fa', markerColor: '#002b36', iconColor: '#eee8d5'}) }).addTo(map)
 
-L.marker([48.15391,11.52283], {icon: L.VectorMarkers.icon({icon: 'rocket', prefix: 'fa', markerColor: '#eee8d5', iconColor: '#002b36'}) }).addTo(map)
+new Marker([48.15391,11.52283], {icon: new Icon({icon: 'rocket', prefix: 'fa', markerColor: '#eee8d5', iconColor: '#002b36'}) }).addTo(map)
