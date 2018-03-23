25fb411b2efa26940a5253c201733320874c231b
andy.rothwell <rothwell.andy@gmail.com>
2018-03-23 15:28:38 -0400

    Don't compile Sass in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 /* eslint-disable */
 
 var path = require('path');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var autoprefixer = require('autoprefixer');
 
 module.exports = {
   output: {
@@ -22,20 +20,6 @@ module.exports = {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loader: 'babel'},
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract(
-          'style',
-          'css!postcss-loader!sass'
-        )
-      }
     ]
   },
-  plugins: [
-    new ExtractTextPlugin("leaflet-vector-markers.css")
-  ],
-  sassLoader: {
-    includePaths: [path.resolve(__dirname, "./src")]
-  },
-  postcss: [ autoprefixer({ browsers: ['last 3 version', '> 10%', 'IE 8'] }) ]
 };
